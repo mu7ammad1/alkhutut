c5b4b3d8cf9bd71412f186b2119673df7db6e631
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2024-11-08 19:41:25 +0200

    1

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button } from "@/components/ui/button";
 import { Download, Lamp } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
