e500883f69db7e1c3ffe98f4c49e42177ef0b769
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2024-11-09 10:06:09 +0200

    1

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,17 +1,20 @@
 import { sql } from "@vercel/postgres";
-import PetsWithPagination from "./useVisit"; // استيراد الكومبوننت الجديد
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
 export default async function Pets() {
   // استعلام لجلب جميع الحيوانات الأليفة من الجدول
   const { rows } = await sql`
     SELECT * FROM Pets
   `;
-
+  const PetsWithPagination = dynamic(() => import("./useVisit"), {
+    loading: () => <p>Loading...</p>,
+  });
   return (
-    <div>
-      <h1>حيوانات أليفة</h1>
-      {/* تمرير البيانات إلى كومبوننت PetsWithPagination */}
-      <PetsWithPagination rows={rows} />
-    </div>
+    <section>
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <PetsWithPagination rows={rows} />
+      </Suspense>
+    </section>
   );
 }
